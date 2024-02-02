c829d995fc123d367d33c96231ce0a629a3705a8
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-02-02 12:52:52 +0200

    feat: add start and end phrase

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import  {getUserName, getGreeting, getfarewall}  from "./modules/greeting/index.js";
+import readline from "readline";
 
-const start = () => {
-    let userName = "";
-    let greeting = "Welcome to the File Manager, ";
-    const args = process.argv;
-    for (let i = 0; i < args.length; i++)
-        if (args[i].startsWith("--") && args[i].includes("="))
-            userName = args[i].split("=")[1];
-    console.log(greeting + userName + "!");
+const start = async () => {
+   let userName = await getUserName();
+   let greeting = await getGreeting();
+   let farewall = await getfarewall();
+
+
+   console.log(userName);
+   console.log(greeting);
+   console.log(farewall);
 };
 
-start();
+await start();
