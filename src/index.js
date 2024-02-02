de3dd985a233afd021d51e99402219c093fc2674
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-02-02 14:52:53 +0200

    feat: add end show working directory

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-import  {getGreeting, getfarewall}  from "./modules/greeting/index.js";
+import { getGreeting, getfarewall } from "./modules/greeting/index.js";
 import readline from "readline/promises";
-let rl = readline.createInterface( process.stdin, process.stdout);
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+let rl = readline.createInterface(process.stdin, process.stdout);
 
 const start = async () => {
-   let greeting = await getGreeting();
-   let farewall = await getfarewall();
+    let greeting = await getGreeting();
+    let farewall = await getfarewall();
 
-   console.log(greeting);
+    console.log(greeting);
 
-   rl.on('line', (text) => {
-       if (text === '.exit')
-           rl.close();
-   });
+    rl.on('line', (text) => {
+        if (text === '.exit')
+            rl.close();
+        else
+            process.stdout.write(`You are currently in ${__dirname}\n`);
+    });
 
-   rl.on('close', () => {
-       console.log(farewall);
-   });
+    rl.on('close', () => {
+        console.log(farewall);
+    });
 
 };
 
