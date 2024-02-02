d9b6d7cdc360e82a4a566a0c602ee908c4e8e49b
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-02-02 12:25:15 +0200

    feat: get name from script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("connection");
+
+const start = () => {
+    let userName = "";
+    let greeting = "Welcome to the File Manager, ";
+    const args = process.argv;
+    for (let i = 0; i < args.length; i++)
+        if (args[i].startsWith("--") && args[i].includes("="))
+            userName = args[i].split("=")[1];
+    console.log(greeting + userName + "!");
+};
+
+start();
