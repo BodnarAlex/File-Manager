01dc65d4451d86551368a1b04ee8c6da137dd57e
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-02-02 14:47:34 +0200

    feat: plug readline

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import  {getUserName, getGreeting, getfarewall}  from "./modules/greeting/index.js";
-import readline from "readline";
+import  {getGreeting, getfarewall}  from "./modules/greeting/index.js";
+import readline from "readline/promises";
+let rl = readline.createInterface( process.stdin, process.stdout);
 
 const start = async () => {
-   let userName = await getUserName();
    let greeting = await getGreeting();
    let farewall = await getfarewall();
 
-
-   console.log(userName);
    console.log(greeting);
-   console.log(farewall);
+
+   rl.on('line', (text) => {
+       if (text === '.exit')
+           rl.close();
+   });
+
+   rl.on('close', () => {
+       console.log(farewall);
+   });
+
 };
 
 await start();
