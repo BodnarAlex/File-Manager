df99ad6e69db2c5b0ecc5839b9720d86a410cede
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-02-03 13:52:15 +0200

    refactor: delete useless code

--- src/modules/ls/show.js
+++ src/modules/ls/show.js
@@ -1,23 +1,19 @@
 import fs from "fs/promises";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const showTable = async (__dirname) => {
+const showTable = async (dirname) => {
     try {
-
         let resCatalog = [];
         let resFiles = [];
-        const files = await fs.readdir(__dirname, { withFileTypes: true , recursive: true});
+        const files = await fs.readdir(dirname, { withFileTypes: true, recursive: true });
         files.forEach(file => {
             if (file.isFile())
                 resFiles.push(file.name);
             else
                 resCatalog.push(file.name);
         });
-        resFiles = resFiles.sort().map((x)=>x = {"Name": x, "Type": "file"});
-        resCatalog = resCatalog.sort().map((x)=>x = {"Name": x, "Type": "directory"});;
-        resCatalog.push(...resFiles);
-        console.table(resCatalog);
+        resFiles = resFiles.sort().map((x) => x = { "Name": x, "Type": "file" });
+        resCatalog = resCatalog.sort().map((x) => x = { "Name": x, "Type": "directory" });;
+        console.table([...resCatalog, ...resFiles]);
     } catch {
         throw new Error("Operation failed");
     }
