61a8496b1a4ffcb2eb97af6ef69b9df4f31c1b27
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-02-03 17:51:33 +0200

    feat: add script for get hash

---
+++ src/modules/hash/index.js
@@ -0,0 +1,21 @@
+import path from "path";
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+
+const calculateHash = async (dirname, filename) => {
+    const pathRead = path.resolve(dirname,filename);
+    const stream = createReadStream(pathRead);
+    const hash = createHash("sha256");
+
+    let data = "";
+    stream.on("data", (chunk) => (data += chunk));
+    stream.on("end", () => {
+        const resultString = hash.update(data).digest('hex');
+        console.log(resultString);
+    });
+    stream.on("error", (error) => console.log("Error", error.message));
+};
+
+export{
+    calculateHash
+}
