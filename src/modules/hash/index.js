13275dad3883d479622a3fe621bb6bb19ecba84d
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-02-05 23:56:09 +0200

    fix: function hash more async

--- src/modules/hash/index.js
+++ src/modules/hash/index.js
@@ -1,22 +1,23 @@
 import path from "path";
 import { createHash } from "crypto";
-import { createReadStream } from "fs";
+import fsPromise from "fs/promises";
 import help from "../startManagement/index.js"
 
 const calculateHash = async (dirname, filename) => {
     if (await help.checkArg(filename.length, 1)) {
 
-        const pathRead = path.resolve(dirname, filename[0]);
-        const stream = createReadStream(pathRead);
-        const hash = createHash("sha256");
-
-        let data = "";
-        stream.on("data", (chunk) => (data += chunk));
-        stream.on("end", () => {
-            const resultString = hash.update(data).digest('hex');
-            console.log(resultString);
-        });
-        stream.on("error", (error) => console.error("Operation failed"));
+        try {
+            const pathRead = path.resolve(dirname, filename[0]);
+            const stats = await fsPromise.stat(pathRead);
+            if (stats.isFile()) {
+                const stream = await fsPromise.readFile(pathRead);
+                const hash = createHash("sha256");
+                const resultString = hash.update(stream).digest('hex');
+                console.log(resultString);
+            }
+        } catch (error) {
+            console.error("Operation failed");
+        }
     }
 };
 
